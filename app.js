ca485b2d886acb9183518f17e92a7f062a9be866
Rachel - Windows <rachenn@gmail.com>
2020-02-16 15:47:23 +0800

    pussshing

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+var express = require ("express");
+var app = express();
 
-const app = express();
+app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 9000;
 
-app.get('/', (req, res) => {
-  res.send("Hello world, Looking to automate this stuff on heroku..! Weee");
-})
+app.set("view engine", "ejs");
+app.use(express.static('styles'));
+app.use(express.static('script'));
+
+app.get("/", function(req, res){
+  res.render('index');
+  });
 
 app.listen(port);
 console.log("Server started at port " + port);
