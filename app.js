fc52c62e4565d14f8cb3ea15ac9f48b470c3f676
Rachel - Windows <rachenn@gmail.com>
2020-02-21 17:58:16 +0800

    Moon Crystals!!!

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require ("express");
 var app = express();
+// var AOS = require("aos");
+
+// import AOS from 'aos';
+// AOS.init();
 
 app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 9000;
@@ -12,6 +16,10 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
   res.render('index');
   });
+
+app.get("/mooncrystals", function(req, res){
+  res.render('moonstone');
+});
 
 app.listen(port);
 console.log("Server started at port " + port);
