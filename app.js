7d89ec961e5971945bfdb065a328efe2478a25d6
Rachel - Windows <rachenn@gmail.com>
2020-02-23 17:40:06 +0800

    .

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require ("express");
 var app = express();
+var mongoose = require("mongoose");
 
 app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 9000;
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb://heroku_b1vf181k:57famg2vp6ge1v389b0fnc1dar@ds217548.mlab.com:17548/heroku_b1vf181k");
 
 app.set("view engine", "ejs");
 app.use(express.static('styles'));
 app.use(express.static('script'));
 app.use(express.static("public"));
+
+// DATABASING
+var workSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	link: String
+});
+
+var test = new test ({
+    name: "Mrs Norris",
+    image: "google.com",
+    breed: "facebook.com"
+})
 
 app.get("/", function(req, res){
   res.render('index');
@@ -19,6 +37,10 @@ app.get("/mooncrystals", function(req, res){
 
 app.get("/maylandsmeadery", function(req, res){
   res.render("maylandsmeadery");
+})
+
+app.get("/*", function(req, res){
+  res.send("You lost?");
 })
 
 app.listen(port);
