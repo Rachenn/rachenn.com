b0d33b05525b535ca1eed005ee1b8308dd8c1586
Rachel - Windows <rachenn@gmail.com>
2022-02-06 13:25:08 +0800

    fixes

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var express = require ("express");
 var app = express();
-var mongoose = require("mongoose");
 
 app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 9000;
-
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.connect("mongodb://heroku_b1vf181k:57famg2vp6ge1v389b0fnc1dar@ds217548.mlab.com:17548/heroku_b1vf181k");
 
 app.set("view engine", "ejs");
 app.use(express.static('styles'));
