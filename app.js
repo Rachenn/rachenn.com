5968a95a5cfd5eb2c59c5231d612c1cfd05e2f3a
Rachel - Windows <rachenn@gmail.com>
2022-02-06 12:12:26 +0800

    removing react to create seperate link

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require ("express");
 var app = express();
 var mongoose = require("mongoose");
-var port = 3000;
+var port = 8080;
 
 app.use(express.static(__dirname + '/public'));
 
